39ddde46bb040fb29aed53f810ef45c6b5986f7a
sairam224 <rampotharlanka2224@gmail.com>
2023-04-30 19:57:34 +0530

    Final Configuration Is Done

--- app.py
+++ app.py
@@ -1,63 +1,43 @@
-from flask import Flask,request,render_template
-import numpy as np
+from flask import Flask, request, render_template
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
-import csv
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-# Open the CSV file
-with open('artifacts/train.csv', 'r') as csv_file:
-    reader = csv.DictReader(csv_file)
-    
-    # Extract unique player names
-    player_names = set(row['Player_Name'] for row in reader)
-    
-    # Generate HTML for the select dropdown
-    select_options = '\n'.join('<option value="{name}">{name}</option>'.format(name=name) for name in player_names)
+application = Flask(__name__)
 
-# Insert the select dropdown into the HTML form
-select_dropdown = '''
-<div class="mb-3">
-    <label class="form-label">Player Name</label>
-    <select class="form-control" name="Player_Name" placeholder="Select Your Player" required>
-        <option class="placeholder" selected disabled value="">Select a player</option>
-        {options}
-    </select>
-</div>
-'''.format(options=select_options)
+app = application
 
-application=Flask(__name__)
+# Read the CSV file and get the unique values in the Player_name column
+data = pd.read_csv('artifacts/train.csv')
+player_names = list(data['Player_Name'].unique())
 
-app=application
 @app.route('/')
 def index():
     return render_template('ind.html')
 
-@app.route('/iplpredict',methods=['GET','POST'])
+@app.route('/iplpredict', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
+    if request.method == 'GET':
+        return render_template('home.html', player_names=player_names)
     else:
-        data=CustomData(
+        data = CustomData(
             Player_Name=request.form.get('Player_Name'),
             Role_Desc=request.form.get('Role_Desc'),
             Player_team=request.form.get('Player_team'),
             Opposit_Team=request.form.get('Opposit_Team'),
             is_manofThematch=request.form.get('is_manofThematch'),
             City_Name=request.form.get('City_Name'),
-            
-
         )
 
-        pred_df=data.get_data_as_data_frame()
+        pred_df = data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
 
-        predict_pipeline=PredictPipeline()
+        predict_pipeline = PredictPipeline()
         print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
+        results = predict_pipeline.predict(pred_df)
+
         print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+        return render_template('home.html', results=results[0], player_names=player_names)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
